0b0c6e80df1db9702d78beeff78bec220cbf4222
Someesvaar <someesvaar.v@gmail.com>
2026-04-16 12:48:00 +0530

    Second commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Someesvaar V" );
+        System.out.println( "1BI23CS210" );
+    }
+}
